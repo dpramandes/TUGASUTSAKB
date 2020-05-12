5bc92dd0d418e75ee12ba4ed62c2e833f108e935
dpramandes <65226921+dpramandes@users.noreply.github.com>
2020-05-12 23:17:09 +0700

    Add files via upload

---
+++ src/main/java/saxobyte/web/utsakb/DuaFragment.java
@@ -0,0 +1,33 @@
+package saxobyte.web.utsakb;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Tanggal Pembuatan : 8 Mei 2020
+// Nama  : Dzicko Pasuci Ramandes
+// NIM   : 10117247
+// Kelas : IF-8
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DuaFragment extends Fragment {
+
+
+    public DuaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dua, container, false);
+    }
+
+}
